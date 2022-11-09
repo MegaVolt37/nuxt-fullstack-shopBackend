232b099c5911c9aec2b51ce04a53a154141027a3
Smirnov Alexandr <alexrus37reg@gmailcom>
2022-11-09 19:51:04 +0300

    запрос для каталога

--- server/index.js
+++ server/index.js
@@ -9,16 +9,18 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const posts = require('./routes/api/posts');
+const catalog = require('./routes/api/catalog');
 app.use('/api/posts', posts);
+app.use('/api/catalog', catalog);
 
 if (process.env.NODE_ENV === 'production') {
   // Static
   app.use(express.static(__dirname + 'public'));
 
-  app.get(/.*/, (req,res) => res.sendFile(__dirname + 'public/index.html'))
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + 'public/index.html'))
 }
 
-  const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server started port-${port}`)
---
+++ server/routes/api/catalog.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get catalog
+router.get('/', async (request, response) => {
+  const catalog = await loadPostCollection();
+  console.log(catalog)
+  response.send(await catalog.find({}).toArray());
+})
+// Add catalog
+router.post('/', async (request, response) => {
+  const catalog = await loadPostCollection();
+  await catalog.insertOne({
+    name: request.body.name,
+    description: request.body.description,
+    price: request.body.price,
+    count: request.body.count,
+    image: request.body.image,
+    createdAt: new Date()
+  });
+  response.status(201).send({ sucess: true });
+})
+// Delete catalog
+router.delete('/:id', async (request, response) => {
+  const catalog = await loadPostCollection();
+  await catalog.deleteOne({ _id: new mongodb.ObjectId(request.params.id) });
+  response.status(200).send({ sucess: true });
+})
+// Patch catalog
+
+
+async function loadPostCollection() {
+  const client = await mongodb.MongoClient.connect('mongodb+srv://alexandr0095:08SA108ser@cluster0.exetvsg.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+  }).catch((e) => console.log(e))
+  return client.db('fullStack-Nuxt3').collection('catalog');
+}
+module.exports = router;
